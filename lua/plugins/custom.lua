cee4badc8e45829a75dc37f44590c359924fcd1c
BADINI Rachid Rodrigue <bsrodrigue@gmail.com>
2024-07-25 14:27:02 +0000

    first commit

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,11 @@
+return {
+  -- provide automatic htmx tag closing
+  {
+    "alvan/vim-closetag",
+  },
+
+  -- provide better javascript autocompletion
+  {
+    "pangloss/vim-javascript",
+  },
+}
